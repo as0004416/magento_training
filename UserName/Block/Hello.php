d10351ce5213ac0dbad74b15db1e6ee531f52fdf
Маковская Кристина <33231232+as0004416@users.noreply.github.com>
2021-03-24 21:45:31 +0300

    Task-2

---
+++ UserName/Block/Hello.php
@@ -0,0 +1,8 @@
+<?php
+namespace Amasty\UserName\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Hello extends Template{
+
+}
---
+++ UserName/registration.php
@@ -0,0 +1,3 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Amasty_UserName',__DIR__);
